5bce0c8ddcd0eda5e0778d2b0e8b4daff10a640d
Michael Aldridge <maldridge@Earth>
2013-10-09 23:21:20 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import logging
+import timing
+import matchdb
+import GUI
+import wx
+
+class RASScoreboard(GUI.MainWindow):
+    def matchAssignPenalty(self):
+        print "made it this far"
+
+if __name__ == "__main__":
+    theapp = RASScoreboard()
