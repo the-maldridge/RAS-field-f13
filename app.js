4e3d361ed347bcea5a1312059204751baadd8db7
Michael Aldridge <maldridge@Earth>
2013-10-29 01:04:16 -0500

    added index file, built static webserver

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.compress());
+app.use('/static', express.static(__dirname + '/static'));
 
 app.listen(process.env.PORT || 3000);
