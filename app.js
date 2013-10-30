fb8dbebcc10b3661a6d1e5895d012c94a6e85399
Michael Aldridge <maldridge@Earth>
2013-10-30 01:08:48 -0500

    added json loader

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
+var fs = require('fs');
+
 var express = require('express');
 var app = express();
 
+function loadDB() {
+    var file = __dirname + '/match.db';
+
+    fs.readFile(file, 'utf8', parseJSON);
+
+}
+
+function parseJSON(err, data){ 
+    if (err) {
+	console.log('Error loading json: ' + err);
+    }
+    _data=JSON.parse(data);
+    console.log('JSON loaded: ' + _data);
+}
+
+function dumpdb(req, res) {
+    res.send(JSON.stringify(_data) + " data concludes");
+}
+
 app.use(express.compress());
 app.use('/static', express.static(__dirname + '/static'));
+loadDB();
+app.get('/jsondump', dumpdb);
+
 
 app.listen(process.env.PORT || 3000);
+console.log("Server started on port: " + (process.env.PORT || 3000));
