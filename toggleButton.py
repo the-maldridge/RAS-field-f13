206a5d494d98e6d6ffea214f3a43669c294a09a5
geppettodivacin <ericdilmore@gmail.com>
2013-11-14 16:24:23 -0600

    Change the ToggleButton style to match Button.

--- toggleButton.py
+++ toggleButton.py
@@ -2,17 +2,16 @@ import curses
 
 class ToggleButton:
     def __init__ ( self, parent, y, x, length, text1, text2 ):
-        self.__dict__['win'] = parent.derwin ( 1, length + 1, y, x )
+        self.__dict__['win'] = parent.derwin ( 3, length + 2, y, x )
         self.text1 = text1
         self.text2 = text2
 
         self.text = text1
 
-        self.win.standout()
-
         self.draw()
     
     def toggle ( self ):
+        curses.curs_set ( 0 )
         if self.text == self.text1:
             self.text = self.text2
         else:
@@ -20,14 +19,18 @@ class ToggleButton:
         self.draw()
 
     def draw ( self ):
-        self.win.addstr ( 0, 0, self.text )
+        self.win.standend()
+        self.win.box()
+        if ( self.text == self.text2 ):
+            self.win.standout()
+        self.win.addstr ( 1, 1, self.text )
         self.win.refresh()
 
     def __getattr__ ( self, attr ):
         return getattr ( self.win, attr )
 
 def main ( screen ):
-    tButton = ToggleButton ( screen, 5, 10, 4, "Foo", "Bar" )
+    tButton = ToggleButton ( screen, 5, 10, 3, "Foo", "Bar" )
     screen.vline ( 0, 80, '|', 80 )
     while ( True ):
         myin = screen.getch()
