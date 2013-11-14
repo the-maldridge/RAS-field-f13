58aefad32aeb8f0bf70c8cc3c3f5b6ba21f278f0
geppettodivacin <ericdilmore@gmail.com>
2013-11-14 13:03:49 -0600

    Flip the constructor x and y to match curses. It should now be called (y,x) instead of (x,y).

--- button.py
+++ button.py
@@ -1,6 +1,6 @@
 import curses
 class Button:
-    def __init__ ( self, parent, x, y, text = "" ):
+    def __init__ ( self, parent, y, x, text = "" ):
         self.__dict__['win'] = parent.derwin ( 3, len ( text ) + 2, y, x )
         self.win.box()
         self.win.addstr ( 1, 1, text )
