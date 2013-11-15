c2ed5fd2edf232a7d81bbb7ed7e4e3d3c8e10f5c
geppettodivacin <ericdilmore@gmail.com>
2013-11-14 22:13:15 -0600

    Add MainWindow.

---
+++ mainWin.py
@@ -0,0 +1,48 @@
+import curses
+import button
+import toggleButton
+import textLabel
+import timeBox
+
+class mainWindow:
+    def __init__ ( self ):
+        self.__dict__['win'] = curses.newwin ( 0, 0 )
+        self.tLabel = textLabel.TextLabel ( self, 1, 0, "Label" )
+        self.tButton = toggleButton.ToggleButton ( self, 1, 10, 3, "Foo", "Bar" )
+        self.startButton = button.Button ( self, 5, 1, "Start" )
+        self.stopButton = button.Button ( self, 5, 10, "Stop" )
+        self.resetButton = button.Button ( self, 5, 19, "Reset" )
+        self.tBox = timeBox.TimeBox ( self, 20, 5 )
+
+    def update ( self ):
+        self.tLabel.draw()
+        self.startButton.draw()
+        self.stopButton.draw()
+        self.resetButton.draw()
+        self.tButton.draw()
+        self.tBox.update()
+
+        self.tLabel.refresh()
+        self.tButton.refresh()
+        self.startButton.refresh()
+        self.stopButton.refresh()
+        self.resetButton.refresh()
+        self.tBox.refresh()
+
+    def __getattr__ ( self, attr ):
+        return getattr ( self.win, attr )
+
+def main ( screen ):
+    m = mainWindow()
+    screen.timeout ( 10 )
+    while ( True ):
+        myin = screen.getch()
+        if myin == curses.KEY_HOME:
+            break
+        elif myin == ord ( 'j' ):
+            m.tButton.toggle()
+        else:
+            m.update()
+
+if __name__ == "__main__":
+    curses.wrapper ( main )
