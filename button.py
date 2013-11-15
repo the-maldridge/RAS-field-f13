c1031238bde8505192c883a55794ba215109f693
geppettodivacin <ericdilmore@gmail.com>
2013-11-14 22:11:43 -0600

    Reorganize Button with draw function.

--- button.py
+++ button.py
@@ -2,8 +2,12 @@ import curses
 class Button:
     def __init__ ( self, parent, y, x, text = "" ):
         self.__dict__['win'] = parent.derwin ( 3, len ( text ) + 2, y, x )
+        self.text = text
+        self.draw()
+
+    def draw ( self ):
         self.win.box()
-        self.win.addstr ( 1, 1, text )
+        self.win.addstr ( 1, 1, self.text )
 
     def __getattr__ ( self, attr ):
         return getattr ( self.win, attr )
