eb4832499bc2492b1dc3c3b26745b65f8196ad52
geppettodivacin <ericdilmore@gmail.com>
2013-11-24 00:03:16 -0600

    Reimplement textArea with subWindow.

--- textArea.py
+++ textArea.py
@@ -1,14 +1,14 @@
 import curses
+import subWin
 
-class TextArea:
+class TextArea ( subWin.SubWindow ):
     def __init__ ( self, parent, y, x, height = None, width = None, text = "" ):
         if ( height is not None and width is not None ):
-            self.__dict__['win'] = parent.derwin ( height, width, y, x )
+            subWin.SubWindow.__init__( self, parent, y, x, height, width )
         else:
-            self.__dict__['win'] = parent.derwin ( y, x )
+            subWin.SubWindow.__init__( self, parent, y, x )
         self.text = text
         self.curline = 0
-        self.update()
 
     def update ( self ):
         height, width = self.win.getmaxyx()
@@ -18,17 +18,14 @@ class TextArea:
         self.win.box()
         for i, line in enumerate ( self.text.splitlines()[ self.curline:self.curline + height - 2 ] ):
             self.win.addstr ( i + 1, 1, line )
-        self.win.refresh()
+
+        subWin.SubWindow.update ( self )
     
     def scroll ( self, dist ):
         if ( dist > 0 and self.curline + dist < len ( self.text.splitlines() ) ):
             self.curline = self.curline + dist
         elif ( dist < 0 and self.curline + dist >= 0 ):
             self.curline = self.curline + dist
-        self.update()
-
-    def __getattr__ ( self, attr ):
-        return getattr ( self.win, attr )
 
 def main ( screen ):
     tArea = TextArea ( screen, 1, 1, 24, 80, 
@@ -42,6 +39,8 @@ def main ( screen ):
             tArea.scroll ( -1 )
         elif myin == curses.KEY_HOME:
             break
+        tArea.update()
+        screen.refresh()
 
 if __name__ == "__main__":
     curses.wrapper ( main )
